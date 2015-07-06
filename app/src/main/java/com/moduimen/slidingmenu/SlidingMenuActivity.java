954a8c0c7bf8b317ca938ca3adccbfc91996763a
xht <xionght11@gmail.com>
2015-07-06 09:36:20 +0800

    slidingmenu

---
+++ app/src/main/java/com/moduimen/slidingmenu/SlidingMenuActivity.java
@@ -0,0 +1,38 @@
+package com.moduimen.slidingmenu;
+
+import com.moduimen.slidingmenu.view.SlidingMenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+
+import com.moduimen.moduitest.R;
+
+public class SlidingMenuActivity extends Activity {
+
+    private SlidingMenu mLeftMenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_slidingmenu);
+        mLeftMenu = (SlidingMenu) findViewById(R.id.id_menu);
+
+    }
+
+    public void toggleMenu(View view) {
+
+        mLeftMenu.toggle();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_sliding_menu, menu);
+        return true;
+    }
+
+}
